2fb69accb39392dc3e294a84f9fafe70a1d73817
techtobit <nurthern2016@gmail.com>
2024-09-23 06:00:00 +0600

    Fixed typescript error

--- src/utils/API_SDK.tsx
+++ src/utils/API_SDK.tsx
@@ -1,3 +1,3 @@
-import axios from "axios";
+// import axios from "axios";
 
-const BASE_URL='https://household-backend-a4k7.onrender.com/';
+// const BASE_URL='https://household-backend-a4k7.onrender.com/';
